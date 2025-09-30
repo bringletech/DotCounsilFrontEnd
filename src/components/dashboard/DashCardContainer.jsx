7f8e9fd0901d962fa5233878099e1308eb8e1d5d
Tushar garg <tushargarg50797@gmail.com>
2025-09-30 12:10:42 +0530

    new

--- src/components/dashboard/DashCardContainer.jsx
+++ src/components/dashboard/DashCardContainer.jsx
@@ -1,38 +1,36 @@
-import React, { useState } from 'react'
-import { cardDetail } from '../../constants/constants'
-import DashCardSmall from './DashCardSmall'
+// import React from 'react'
+// import { cardDetail, analysisCardDetail } from '../../constants/constants'
+// import DashCardSmall from './DashCardSmall'
 
+// function DashCardContainer({ type, stats, analysisStats }) {
+//   // array select kro type ke hisaab se
+//   const cards = type === "dash" ? cardDetail : analysisCardDetail;
 
-function DashCardContainer() {
+//   return (
+//     <div className="cards w-full flex gap-2">
+//       {cards.map((card, idx) => {
+//         let digits = 0;
 
-    const [stats,setStats]=useState({
-         totalUsers: 2000,
-         activeUsers: 1500,
-         totalCourses: 25,
-         revenue: 50000,
-    })
-    const [analysisStats,setAnalysisStats]=useState({
-       
-    })
-    
-  return (
-    <>
-    <div className="cards w-full  flex gap-2">
-                       {
-                        
-                        cardDetail.map((card)=>{
-                            let digits=0;
-                            if(card.title==="Total Users") digits=stats.totalUsers;
-                            if(card.title==="Active Users") digits=stats.activeUsers;
-                            if(card.title==="Total Courses") digits=stats.totalCourses;
-                            if(card.title==="Revenue") digits=stats.revenue;
-                            
-                            return <DashCardSmall {...card} digits={digits}></DashCardSmall>
-                        })
-                        }
-     </div>
-    </>
-  )
-}
+//         // dashboard condition
+//         if (type === "dash") {
+//           if (card.title === "Total Users") digits = stats.totalUsers;
+//           if (card.title === "Active Users") digits = stats.activeUsers;
+//           if (card.title === "Total Courses") digits = stats.totalCourses;
+//           if (card.title === "Revenue") digits = stats.revenue;
+//         }
 
-export default DashCardContainer
+//         // analysis condition
+//         if (type === "analysis") {
+//           if (card.title === "Total Revenue") digits = analysisStats.TotalRevenue;
+//           if (card.title === "Total Enrollment") digits = analysisStats.TotalEnrollment;
+//           if (card.title === "Completion Rate") digits = analysisStats.completionRate;
+//           if (card.title === "Avg Revenue User") digits = analysisStats.AvgRevenueUser;
+//         }
+
+//         return <DashCardSmall key={idx} {...card} digits={digits} />
+//       })}
+//     </div>
+//   )
+// }
+
+// export default DashCardContainer
